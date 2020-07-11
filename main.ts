d1704ebe194cbed84f24f6fa1116c1664bb4dddb
Rasmus Lauridsen <r.h.lauridsen@hotmail.dk>
2020-07-11 19:56:12 +0200

    Finished up code

--- main.ts
+++ main.ts
@@ -1,35 +1,42 @@
 input.onButtonPressed(Button.A, function () {
+    led.plot(2, 2)
     if (number_arr[pos] == 0) {
-        list[pos] = 1
+        number_arr[pos] = 1
     } else {
-        list[pos] = 0
+        number_arr[pos] = 0
     }
+    basic.pause(100)
+    led.unplot(2, 2)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
-    for (let index = 0; index <= 5; index++) {
-        if (list[index] == 1) {
+    for (let index = 0; index <= 8; index++) {
+        if (number_arr[index] == 1) {
             number += 2 ** index
         }
     }
     if (number != 0) {
-        for (let index = 0; index < 2; index++) {
-            basic.showNumber(number * 5)
-        }
+        basic.showNumber(number * 5)
     }
+    reset()
 })
 input.onButtonPressed(Button.B, function () {
-    if (pos < 5) {
+    led.plot(2, 2)
+    if (pos < 8) {
         pos += 1
     } else {
         pos = 0
     }
+    basic.pause(100)
+    led.unplot(2, 2)
 })
-let list: number[] = []
-let number_arr: number[] = []
-let pos = 0
+function reset () {
+    number = 0
+    pos = 0
+    number_arr = [0, 0, 0, 0, 0, 0, 0, 0]
+    basic.showIcon(IconNames.Diamond)
+}
 let number = 0
-number = 0
-pos = 0
-number_arr = [0, 0, 0, 0, 0]
-basic.showIcon(IconNames.SmallDiamond)
+let pos = 0
+let number_arr: number[] = []
+reset()
